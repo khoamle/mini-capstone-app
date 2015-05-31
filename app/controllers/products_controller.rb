dc316f1b8ad8343ab752d76a78a22c11a1c4e203
Khoa Le <khoaminhle5@gmail.com>
2015-05-31 16:56:36 -0500

    added show action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
-  def single
+  def show
+    @product = Product.find_by(id: params[:id])
   end
 end
