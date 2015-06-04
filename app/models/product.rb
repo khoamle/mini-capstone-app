4ed3e9b9f3b9118f218d038e41d7de6495bc03c1
Khoa Le <khoaminhle5@gmail.com>
2015-06-03 21:03:45 -0500

    add model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,30 @@
 class Product < ActiveRecord::Base
+  SALES_TAX = 0.09
+  def friendly_updated_at
+    updated_at.strftime("%b %d, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hours"
+    "#{hours} hours, #{minutes}, minutes"
+  end
+
+  def sale_message
+    if price.delete("$").to_i < 5
+      "Discount Item!"
+    else
+      "On Sale!"
+    end 
+  end
+
+  def tax
+    price.delete("$").to_i * SALES_TAX
+  end
+
+  def total
+    price.delete("$").to_i + tax
+  end
 end
