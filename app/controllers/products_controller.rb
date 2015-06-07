64a0b9afa52c9ccce603d26883f27878191a5e15
Khoa Le <khoaminhle5@gmail.com>
2015-06-07 16:50:56 -0500

    add search features under More FEatures dropdown

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,21 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    if params[:sort] && params[:sort_order]
+      @products = Product.order(params[:sort] => params[:sort_order])
+    end
+    if params[:discount]
+      @products = Product.where("price < ?", 2)
+    end
   end
 
   def show
-    @product = Product.find_by(id: params[:id])
+    if params[:id] == "random"
+      products = Product.all
+      @product = products.sample
+    else
+      @product = Product.find_by(id: params[:id])
+    end
   end
 
   def new
@@ -32,5 +43,10 @@ class ProductsController < ApplicationController
     @product.destroy
     flash[:warning] = "Product successfully deleted!"
     redirect_to "/products"
+  end
+  def search 
+    search_term = params[:search]
+    @products = Product.where("name LIKE ?", "%#{search_term}%")
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "/products/:id/edit" => 'products#edit'
   patch "/products/:id" => 'products#update'
   delete "/products/:id" => 'products#destroy'
-
+  post "/search" => 'products#search' 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
